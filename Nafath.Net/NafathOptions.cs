aad34001e1757df99ea7a7bb5ed87af2ad1707bf
Abdur Rahim <55857512+arahimx@users.noreply.github.com>
2025-05-25 12:19:31 +0300

    Add GitHub Actions CI workflow

--- Nafath.Net/NafathOptions.cs
+++ Nafath.Net/NafathOptions.cs
@@ -1,7 +1,7 @@
 ﻿namespace Nafath.Net;
-    public class NafathOptions
-    {
-        public string BaseUrl { get; set; } = "https://auth.nafath.sa";
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
-    }
+public class NafathOptions
+{
+    public string BaseUrl { get; set; } = "https://auth.nafath.sa";
+    public string ClientId { get; set; }
+    public string ClientSecret { get; set; }
+}
